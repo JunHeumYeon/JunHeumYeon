b059af39000f532014a4c89e6598ef9a44852552
soft <soft@soft-PC>
2016-09-09 11:20:31 +0900

    commit message

--- HelloWorld/src/project1/project2.java
+++ HelloWorld/src/project1/project2.java
@@ -4,6 +4,7 @@ public class project2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("2013041054 ø¨¡ÿ»Ï");
 		// TODO Auto-generated method stub
 
 	}
